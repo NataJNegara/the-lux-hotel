0adb9c68872cfb104885d4d37bc9d82252e6977d
Pranata Jaya Negara <pranatajayanegara@gmail.com>
2024-03-21 18:10:03 +0700

    .

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    " process.env.VITE_SUPABASE_URL": JSON.stringify(
+      process.env.VITE_SUPABASE_URL
+    ),
+    " process.env.VITE_SUPABASE_KEY": JSON.stringify(
+      process.env.VITE_SUPABASE_KEY
+    ),
+  },
 });
