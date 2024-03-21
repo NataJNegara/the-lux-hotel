772491dd31ed2c3522f1b4aa6f5bb4ac754ff288
Pranata Jaya Negara <76525308+NataJNegara@users.noreply.github.com>
2024-03-21 17:13:06 +0700

    Update LoginForm.jsx

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -10,8 +10,8 @@ import { useUser } from "./useUser";
 import { useNavigate } from "react-router-dom";
 
 function LoginForm() {
-  const [email, setEmail] = useState("aerith@gmail.com");
-  const [password, setPassword] = useState("12345678");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { isLogin, login } = useLogin();
 
   const navigate = useNavigate();
